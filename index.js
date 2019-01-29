d276a99529e02d78da3986c735e12a2a3b5601dc
Chris Franklin <cf73021@gmail.com>
2019-01-29 18:44:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function placeOrder(cardNumber) {
       total += cart[i].itemPrice;
       cart.pop();
     }
-    return `Your total cost is \$${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
