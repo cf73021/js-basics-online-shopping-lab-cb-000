2bd228f99980e4254f468e047110e74992e98b81
Chris Franklin <cf73021@gmail.com>
2019-01-29 17:49:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   }
   else {
+    var i = 0
     for (i = 0, len = cart.length; i < len; i++) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`}
   }
