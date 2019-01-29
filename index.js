5f03be594def08ab0db4170023038e9b693d7135
Chris Franklin <cf73021@gmail.com>
2019-01-29 18:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,10 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      
+    }
 }
 
 function placeOrder(cardNumber) {
