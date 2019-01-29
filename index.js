7dc78d19fd3b80e7f86f4369f574fd6e8fb4b368
Chris Franklin <cf73021@gmail.com>
2019-01-29 17:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var cart = [];
 
+function addToCart(item) {
+  var new_addition = {itemName: item, itemPrice: Math.floor(Math.random() * 10)}
+  cart.push(new_addition)
+  return `${item} has been added to your cart.`
+  return cart
+}
+
 function getCart() {
  return cart;
 }
@@ -9,12 +16,15 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
-}
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  else {
+    for (i = 0, len = cart.length; i < len; i++) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`}
+  }
 }
 
 function total() {
