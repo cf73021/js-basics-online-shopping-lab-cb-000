77bf3485a4a23798066698698df43eb989be4d45
Chris Franklin <cf73021@gmail.com>
2019-01-29 18:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   else {
     for (var i = 0; i < cart.length; i++) {
       var return_value = ""
-      if (cart.length === 1) {
+      if (i === 1) {
         return_value += 'In your cart, you have ' + `${cart[i].itemName} at $${cart[i].itemPrice}.`
       }
     }
